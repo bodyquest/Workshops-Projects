4934cabb5507c66389d954b171160e1ad6c7ee07
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-09-08 22:58:17 +0200

    Add Editable List web component

---
+++ JS-Apps-June2020_Web-Components/07.carousel/index.js
@@ -0,0 +1,19 @@
+import {html, render} from "./node_modules/lit-html/lit-html.js";
+import {repeat} from "./node_modules/lit-html/directives/repeat.js";
+
+function template(context) {
+    return html`
+    
+    `;
+}
+
+class Carousel extends HTMLElement {
+
+    constructor() {
+        super();
+
+
+    }
+}
+
+customElements.define("carousel", Carousel);
